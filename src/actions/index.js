5534b5c82eb65b29d7f3a8c14e74749beb58cdfb
Jared <jaredarodriguez@gmail.com>
2019-11-14 19:16:10 -0600

    action creator to get User

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,8 @@ export const fetchPosts = () => async dispatch => {
      dispatch({ type: 'FETCH_POSTS', payload: response.data})
     }; 
  
+export const fetchUser = (id) => async dispatch => {
+     const response = await jsonPlaceholder.get(`/users/`${id})
+
+     dispatch({ type: 'FETCH_USER', payload: response.data }); 
+}; 
