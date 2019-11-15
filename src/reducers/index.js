606282c67adcc1c39c69b08ef0f867ce9af04936
Jared <jaredarodriguez@gmail.com>
2019-11-15 17:11:30 -0600

    refactor user render

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'; 
 import postReducer from './postReducer';
+import userReducer from './usersReducer'; 
 
 export default combineReducers({
-    posts: postReducer
+    posts: postReducer,
+    users: userReducer
 }); 
