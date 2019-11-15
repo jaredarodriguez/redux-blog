97f7d06351323776f542f12570101b59bb6689f5
Jared <jaredarodriguez@gmail.com>
2019-11-15 17:44:03 -0600

    refactor memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,13 +8,12 @@ export const fetchPosts = () => async dispatch => {
      dispatch({ type: 'FETCH_POSTS', payload: response.data})
     }; 
  
-export const fetchUser = id => dispatch => {
-     _fetchUser(id, dispatch); 
-}; 
-
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);  
 const _fetchUser = _.memoize(async (id, dispatch) => {
      const response = await jsonPlaceholder.get(`/users/${id}`)
 
      dispatch({ type: 'FETCH_USER', payload: response.data }); 
 }); 
+
+
 
